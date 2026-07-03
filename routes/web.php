e3ff5b1cf886cf9b061859bd6dd9291e73135a79
Darius Giurgi <darius.giurgi@outlook.com>
2026-07-04 00:26:06 +0300

    Redirect root to app login

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/app/login');
 });
 
 Route::get('/account-suspended', function () {
